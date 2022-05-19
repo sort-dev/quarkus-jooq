73c146fd02291cf0d90de614823ccf54ca03e1bc
apatrida <jayson.minard@gmail.com>
2022-05-19 12:56:54 -0600

    remove unused integ test

--- integration-tests/src/test/java/dev/sort/oss/quarkus/jooq/it/QuarkusJooqResourceIT.java
+++
@@ -1,7 +0,0 @@
-package com.github.sort.dev.quarkus.jooq.it;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-public class QuarkusJooqResourceIT extends QuarkusJooqResourceTest {
-}
